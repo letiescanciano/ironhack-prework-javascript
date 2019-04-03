3a891c2ff46caa582852ef2fb7a224631c0344d6
letiescanciano <leticia.escanciano@gmail.com>
2019-04-03 20:04:44 +0200

    Iteration 4 | Commands

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Rover Object Goes Here
 var rover = {
   direction: 'N',
+  positionX: 0,
+  positionY: 0
 }
 // ======================
 
@@ -31,14 +33,65 @@ function turnLeft(rover) {
 
 function turnRight(rover) {
   console.log("turnRight was called!");
+  console.log("I was facing " + rover.direction);
+  switch (rover.direction) {
+    case "N":
+      rover.direction = 'E';
+      break;
+    case "S":
+      rover.direction = 'W';
+      break;
+    case "E":
+      rover.direction = 'S';
+      break;
+    case "W":
+      rover.direction = 'N';
+      break;
+    default:
+      rover.direction = rover.direction;
+  }
+
+  console.log("Now I'm facing " + rover.direction);
+
 }
 
 function moveForward(rover) {
-  console.log("moveForward was called")
+  console.log("moveForward was called");
+  switch (rover.direction) {
+    case "N":
+      rover.positionY--;
+      break;
+    case "S":
+      rover.positionY++;
+      break;
+    case "E":
+      rover.positionX++;
+      break;
+    case "W":
+      rover.positionX--;
+      break;
+  }
+  console.log("My coordinates are " + rover.positionX + "," + rover.positionY);
+}
+
+function move(command, rover) {
+
+  for (var i = 0; i < command.length; i++) {
+    console.log("iteration number" + i);
+    switch (command[i]) {
+      case 'r':
+        turnRight(rover);
+        break;
+      case 'l':
+        turnLeft(rover);
+        break;
+      case 'f':
+        moveForward(rover);
+        break;
+    }
+  }
+
 }
 
 //Execution tests
-turnLeft(rover);
-turnLeft(rover);
-turnLeft(rover);
-turnLeft(rover);
+move('rffrfflfrff', rover);
